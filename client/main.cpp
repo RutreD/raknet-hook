6863535d5e802092bef59d695b0daaeca4b90b3f
Eduard Babinyan <eduard.babinyan@outlook.com>
2020-05-17 01:24:58 +0300

    Init Project

---
+++ client/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <Windows.h>
+#include <fstream>
+#include <string>
+
+void LOG(std::string text)
+{
+    std::ofstream file("assistant.log");
+    file << text << std::endl;
+    file.close();
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwReason, LPVOID lpReserved)
+{
+    switch (dwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
